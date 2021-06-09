6e7742c8e5cbfe8ff8af0d2e48518416e63cd5e9
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-09 20:28:28 -0300

    post comment

--- src/app.js
+++ src/app.js
@@ -7,31 +7,31 @@ app.use(cors());
 
 const posts = [
     {
-        id: 0,
+        id: 1,
         title: 'Hello World',
         coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
         contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
         content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
         commentCount: 2
     }, {
-        id: 1,
+        id: 2,
         title: 'Teste',
         coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
         contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
         content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
-        commentCount: 2
+        commentCount: 0
     }
 ];
 
 const comments = [
     {
-        id: 0,
-        postId: 0,
+        id: 1,
+        postId: 1,
         author: 'João',
         content: 'Muito bom esse post! Tá de parabéns'
       }, {
-        id: 1,
-        postId: 0,
+        id: 2,
+        postId: 1,
         author: 'Maria',
         content: 'Como faz pra dar palmas?'
       }
@@ -49,7 +49,7 @@ app.get("/posts/:postId", (req, res) => {
 
 app.post("/posts", (req, res) => {
     const post = { 
-        id: posts.length, 
+        id: posts.length + 1, 
         title: req.body.title,
         coverUrl: req.body.coverUrl,
         contentPreview: (req.body.content.slice(0, 55) + "...").replace("<p>", '').replace("</p>", ''),
@@ -64,6 +64,19 @@ app.get("/posts/:id/comments", (req, res) => {
     const id = req.params.id;
     const postComments = comments.filter(e => e.postId === parseInt(id));
     res.send(postComments);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+    const id = req.params.id;
+    const { author, content } = req.body;
+    const comment = {
+        id: comments.length + 1,
+        postId: parseInt(id),
+        author,
+        content
+    };
+    comments.push(comment);
+    res.send(comment);
 });
 
 //START
