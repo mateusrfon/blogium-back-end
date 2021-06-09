d41d07330c0985fe831f5c86b9009662420c6d0f
Mateus Ribeiro <mateusrfon@gmail.com>
2021-06-09 19:25:14 -0300

    post post

--- src/app.js
+++ src/app.js
@@ -7,14 +7,14 @@ app.use(cors());
 
 const posts = [
 {
-    id: 1,
+    id: 0,
     title: 'Hello World',
     coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
     content: 'Este é o conteúdo do post, o que realmente vai aparecer na página do post...',
     commentCount: 2
 },{
-    id: 2,
+    id: 1,
     title: 'Teste',
     coverUrl: 'https://miro.medium.com/max/1024/1*OohqW5DGh9CQS4hLY5FXzA.png',
     contentPreview: 'Esta é a estrutura de um post esperado pelo front-end',
@@ -30,6 +30,19 @@ app.get("/posts", (req, res) => {
 app.get("/posts/:postId", (req, res) => {
     const id = req.params.postId;
     const post = posts.reduce((acc, e) => e.id === parseInt(id) ? acc = e : acc, {});
+    res.send(post);
+});
+
+app.post("/posts", (req, res) => {
+    const post = { 
+        id: posts.length, 
+        title: req.body.title,
+        coverUrl: req.body.coverUrl,
+        contentPreview: req.body.content.slice(0, 55) + "...",
+        content: req.body.content,
+        commentCount: 0
+    };
+    posts.push(post);
     res.send(post);
 });
 
